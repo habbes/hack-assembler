efff137067229e0c52309e0cba6e55ee4543c82b
Habbes <haby_habbes@outlook.com>
2017-02-25 23:19:38 +0300

    Function to clean and extract source command from line of text

--- src/hack_assembler/parser.clj
+++ src/hack_assembler/parser.clj
@@ -1,9 +1,34 @@
-(ns hack-assembler.parser)
+(ns hack-assembler.parser
+  (:require [clojure.string :as s]))
 
 ; regex used to parse a c instruction
 (def c-inst-re #"(?:([AMD]{1,3})\=)?([AMD01+\-]+)(?:;([A-Z]{3}))?")
 
+(declare parse-a-instruction)
+(declare parse-c-instruction)
+
+(defn extract-instruction
+  "extract instruction from the source line, ignoring comments and whitespace.
+  If no instruction is in the supplied line an empty string is returned."
+  [source]
+  (-> source
+      (s/split #"//")                                       ; split instruction and comment
+      first                                                 ; ignore comment
+      s/trim                                                ; remove outer whitespace
+      (s/replace #"\s+" "")                                 ; remove internal whitespace
+      ))
+  
+(defn parse-instruction
+  "parses the source assembly string into a command map
+  containing the type and parts of the instruction"
+  [source]
+  (if (= (subs source 0 1) "@")
+    (parse-a-instruction source)
+    (parse-c-instruction source)))
+
 (defn parse-a-instruction
+  "parses the source assembly into an A Instruction map
+  containing the type A_COMMAND and the address part"
   [source]
   (let [address (subs source 1)]
     {:type    "A_COMMAND"
@@ -11,6 +36,8 @@
 
 
 (defn parse-c-instruction
+  "parses the source assembly string into a C Instruction map
+  containing the type C_COMMAND and the dest, comp and jump parts"
   [source]
   (let [[_ dest comp jump] (re-matches c-inst-re source)]
     {:type "C_COMMAND"
--- test/hack_assembler/parser_test.clj
+++ test/hack_assembler/parser_test.clj
@@ -2,6 +2,33 @@
   (:require [clojure.test :refer :all]
             [hack-assembler.parser :refer :all]))
 
+(deftest extract-instruction-test
+  (testing "instruction only"
+    (let [instruction (extract-instruction "D=1;JMP")]
+      (is (= instruction "D=1;JMP"))))
+  (testing "ignores comments"
+    (let [instruction (extract-instruction "D=1;JMP// this is a comment")]
+      (is (= instruction "D=1;JMP"))))
+  (testing "ignores whitespace"
+    (let [instruction (extract-instruction "  D=1;JMP  ")]
+      (is (= instruction "D=1;JMP")))
+    (let [instruction (extract-instruction "  D=1;  JMP")]
+      (is (= instruction "D=1;JMP")))
+    (let [instruction (extract-instruction "D = 1;JMP")]
+      (is (= instruction "D=1;JMP"))))
+  (testing "ignores comments and whitespace"
+    (let [instruction (extract-instruction "D = 1;JMP // this is a comment")]
+      (is (= instruction "D=1;JMP"))))
+  (testing "returns empty string when no instruction is supplied"
+    (let [instruction (extract-instruction " ")]
+      (is (= instruction "")))
+    (let [instruction (extract-instruction "        ")]
+      (is (= instruction "")))
+    (let [instruction (extract-instruction "// test")]
+      (is (= instruction "")))
+    (let [instruction (extract-instruction "  // test")]
+      (is (= instruction "")))))
+
 (deftest parse-a-instruction-test
   (testing "command type is A_COMMAND"
     (is (= (:type (parse-a-instruction "@256")) "A_COMMAND")))
@@ -35,5 +62,15 @@
       (is (= [dest comp jump] [nil "1" nil])))
     (let [{:keys [dest comp jump]} (parse-c-instruction "D+1")]
       (is (= [dest comp jump] [nil "D+1" nil])))))
+
+(deftest parse-instruction-test
+  (testing "parse A instruction"
+    (let [{:keys [type address]} (parse-instruction "@123")]
+      (is (= type "A_COMMAND"))
+      (is (= address "123"))))
+  (testing "parse C instruction")
+    (let [{:keys [type dest comp jump]} (parse-instruction "D=1")]
+      (is (= type "C_COMMAND"))
+      (is (= [dest comp jump] ["D" "1" nil]))))
 
 (run-tests)
